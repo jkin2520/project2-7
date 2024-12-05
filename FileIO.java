a1046c55525b5aba56087136d5771f3b453f383d
Jalen King <jkin2520@students.vsu.edu>
2024-12-05 06:24:50 -0500

    Implement readFile method

--- FileIO.java
+++ FileIO.java
@@ -1,10 +1,15 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileIO {
     public static void writeToFile(String filename, String content) throws IOException {
         try (FileWriter writer = new FileWriter(filename)) {
             writer.write(content);
         }
+    }
+    public static String readFile(String filename) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filename)));
     }
 }
