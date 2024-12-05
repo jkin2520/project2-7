879853e84dee038ce0749498e914b98ec4b0da3d
Jalen King <jkin2520@students.vsu.edu>
2024-12-05 06:25:21 -0500

    Add unit test for appendToFile

--- FileIOTest.java
+++ FileIOTest.java
@@ -6,7 +6,21 @@ public class FileIOTest {
     @Test
     public void testWriteToFile() throws IOException {
         FileIO.writeToFile("testfile.txt", "Hello, World!");
-        String content = FileIO.readFile("testfile.txt"); // This will throw error as methods don’t exist yet.
+        String content = FileIO.readFile("testfile.txt");
         assertEquals("Hello, World!", content);
     }
+    @Test
+public void testReadFile() throws IOException {
+    FileIO.writeToFile("testfile2.txt", "This is a test");
+    String content = FileIO.readFile("testfile2.txt");
+    assertEquals("This is a test", content);
+}
+@Test
+public void testAppendToFile() throws IOException {
+    FileIO.writeToFile("testfile3.txt", "Hello");
+    FileIO.appendToFile("testfile3.txt", ", World!");
+    String content = FileIO.readFile("testfile3.txt");
+    assertEquals("Hello, World!", content);
+}
+
 }
