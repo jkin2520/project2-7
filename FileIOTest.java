3133b5b07256eeac691a05b7fd1a858b6d325781
Jalen King <jkin2520@students.vsu.edu>
2024-12-05 07:58:15 -0500

    Successfully implemented and tested FileIO functionality with junit 4

--- FileIOTest.java
+++ FileIOTest.java
@@ -1,26 +1,28 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import java.io.IOException;
 
 public class FileIOTest {
+
     @Test
     public void testWriteToFile() throws IOException {
         FileIO.writeToFile("testfile.txt", "Hello, World!");
         String content = FileIO.readFile("testfile.txt");
         assertEquals("Hello, World!", content);
     }
-    @Test
-public void testReadFile() throws IOException {
-    FileIO.writeToFile("testfile2.txt", "This is a test");
-    String content = FileIO.readFile("testfile2.txt");
-    assertEquals("This is a test", content);
-}
-@Test
-public void testAppendToFile() throws IOException {
-    FileIO.writeToFile("testfile3.txt", "Hello");
-    FileIO.appendToFile("testfile3.txt", ", World!");
-    String content = FileIO.readFile("testfile3.txt");
-    assertEquals("Hello, World!", content);
-}
 
+    @Test
+    public void testReadFile() throws IOException {
+        FileIO.writeToFile("testfile2.txt", "This is a test");
+        String content = FileIO.readFile("testfile2.txt");
+        assertEquals("This is a test", content);
+    }
+
+    @Test
+    public void testAppendToFile() throws IOException {
+        FileIO.writeToFile("testfile3.txt", "Hello");
+        FileIO.appendToFile("testfile3.txt", ", World!");
+        String content = FileIO.readFile("testfile3.txt");
+        assertEquals("Hello, World!", content);
+    }
 }
